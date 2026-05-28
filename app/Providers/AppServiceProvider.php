381f13eb8d25ea3a545f658923d536d7c1146f8c
diL <preykerthebest26@gmail.com>
2026-05-28 19:09:25 +0800

    fix: force https for form submissions on production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\URL;
 use App\Models\User;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,5 +25,8 @@ class AppServiceProvider extends ServiceProvider
         Gate::define('admin', function (User $user) {
             return $user->role === 'admin';
         });
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
